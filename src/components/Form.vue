b733042bfa44339ff17a14fce5a23b2be045ef40
Nimisha-12345 <nimishas082@gmail.com>
2021-05-21 12:00:11 +0530

    formatted

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -1,60 +1,57 @@
 <template>
- <div id="submitForm">
-    <p>Type Something..</p> 
-        <i>Press enter</i>
-        <br/>
-        <form v-on:submit.prevent>
-            <input v-on:keypress="submit" type="text" v-model="text" /> 
-        </form>
-</div>
+  <div id="submitForm">
+    <p>Type Something..</p>
+    <i>Press enter</i>
+    <br />
+    <form v-on:submit.prevent>
+      <input v-on:keypress="submit" type="text" v-model="text" />
+    </form>
+  </div>
 </template>
 
 <script>
 export default {
   name: "Form",
-  data: function() {
+  data: function () {
     return {
-      text: ""
+      text: "",
     };
   },
   watch: {
-    name: function(newName) {
-      // eslint-disable-next-line
+    name: function (newName) {
       console.log("watch    ", newName);
       this.text = newName;
-    }
+    },
   },
   props: {
     name: {
       type: String,
-      require: true
+      require: true,
     },
     editIndex: {
       type: Number,
-      require: true
-    }
+      require: true,
+    },
   },
   methods: {
-    submit: function(e) {
-      // eslint-disable-next-line
+    submit: function (e) {
       console.log(this.editIndex);
-      // eslint-disable-next-line
+
       console.log(this.text);
       if (e.keyCode === 13) {
         if (this.editIndex !== -1) {
-          // eslint-disable-next-line
           console.log("edit item");
           this.$emit("edit-item", {
             name: this.text,
-            editIndex: this.editIndex
+            editIndex: this.editIndex,
           });
         } else {
           this.$emit("submit-item", this.text);
         }
         this.text = "";
       }
-    }
-  }
+    },
+  },
 };
 </script>
 
--- src/components/List.vue
+++ src/components/List.vue
@@ -1,18 +1,20 @@
 <template>
-    <div id="testing">
+  <div id="testing">
+    <Form
+      v-bind:edit-index="editIndex"
+      v-bind:name="name"
+      v-on:edit-item="edit"
+      v-on:submit-item="submit"
+    />
+    <ul>
+      <li v-for="(item, index) in data" v-bind:key="index">
+        <span>{{ item }}</span>
 
-        
-        <Form v-bind:edit-index="editIndex" v-bind:name="name" v-on:edit-item="edit" v-on:submit-item="submit" />
-        <ul>
-            <li v-for="(item, index) in data" v-bind:key="index">
-                <span>{{item}}</span>
-
-                <a href="#" v-on:click.prevent="deleteItem(index)" >Delete</a>
-                <a href="#" v-on:click.prevent="editItem(index)" >Edit</a>
-            </li>
-        </ul>
-
-    </div> 
+        <a href="#" v-on:click.prevent="deleteItem(index)">Delete</a>
+        <a href="#" v-on:click.prevent="editItem(index)">Edit</a>
+      </li>
+    </ul>
+  </div>
 </template>
 
 
@@ -22,38 +24,38 @@ import Form from "./Form";
 export default {
   name: "List",
   components: {
-    Form
+    Form,
   },
-  data: function() {
+  data: function () {
     return {
       data: [],
       name: "",
-      editIndex: -1
+      editIndex: -1,
     };
   },
   methods: {
-    submit: function(name) {
+    submit: function (name) {
       this.data.push(name);
       this.name = "";
     },
-    edit: function(obj) {
-      var { name, editIndex} = obj;
+    edit: function (obj) {
+      var { name, editIndex } = obj;
       // eslint-disable-next-line
       console.log("actual edit", name, editIndex);
       Vue.set(this.data, editIndex, name);
       this.name = "";
       this.editIndex = -1;
     },
-    deleteItem: function(index) {
+    deleteItem: function (index) {
       this.data.splice(index, 1);
     },
-    editItem: function(index) {
+    editItem: function (index) {
       // eslint-disable-next-line
       console.log("edit index");
       this.editIndex = index;
       this.name = this.data[index];
-    }
-  }
+    },
+  },
 };
 </script>
 <style>
@@ -66,10 +68,10 @@ export default {
   float: right;
   padding-right: 20px;
 }
-#testing ul{
-    list-style-type: none;
-    max-width: 1300px;
-    margin: 25px auto;
-    font-size: 20px;
+#testing ul {
+  list-style-type: none;
+  max-width: 1300px;
+  margin: 25px auto;
+  font-size: 20px;
 }
 </style>
